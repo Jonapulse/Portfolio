468c6b2c16dea885b0c4b559ad455c09d10f2528
Jonah Pulsipher <jonapulse@gmail.com>
2021-05-19 17:50:05 -0600

    Seriously, it's hell

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -18,6 +18,7 @@ const RepoReadmeLink: React.VFC = () => (
 export const Home: React.VFC = () => (
   <div>
     <P>I'm a game designer and developer in Portland, OR. For serious. I'm not from some weird cult enclave like Utah.</P>
+	<img src="./art/testGifStill.png"/>
 	<div>
 		<InteractiveLink to="/G1LightBorrower">
 		  <HoverGif stillPath="./art/testGifStill.png" gifPath="./art/testgif.gif" title="TestTitle1"> </HoverGif>
