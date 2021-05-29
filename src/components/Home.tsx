47c2bf6c5223d5812c9c5d42434823aa1be5cc6f
Jonah Pulsipher <jonapulse@gmail.com>
2021-05-29 17:42:35 -0600

    Uploading hovergif style

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -3,6 +3,7 @@ import * as CSS from 'csstype';
 import { InteractiveLink } from './InteractiveLink';
 import { Code, P } from './UI';
 import { HoverGif } from './HoverGif';
+import './style/styles.css';
 
 const pageLinkContainerStyle: CSS.Properties = {
   display: 'block',
@@ -18,29 +19,22 @@ const RepoReadmeLink: React.VFC = () => (
 export const Home: React.VFC = () => (
   <div>
     <P>I'm a game designer and developer in Portland, OR. For serious. I'm not from some weird cult enclave like Utah.</P>
-	<div>
+	<div className="gridContainer">
 		<InteractiveLink to="/G1LightBorrower">
 		  <HoverGif stillPath="./art/testGifStill.jpg" gifPath="./art/testgif.gif" title="TestTitle1"> </HoverGif>
 		</InteractiveLink>
-		 | 
 		<InteractiveLink to="/G2HeartburnBoulevard">
 		  <HoverGif stillPath="./art/testGifStill.jpg" gifPath="./art/testgif.gif" title="TestTitle2"> </HoverGif>
 		</InteractiveLink>
-		 | 
 		<InteractiveLink to="/G3Pratfall">
 		  <HoverGif stillPath="./art/testGifStill.jpg" gifPath="./art/testgif.gif" title="TestTitle3"> </HoverGif>Pratfall
 		</InteractiveLink>
-	</div>
-	<br/>
-	<div>
 		<InteractiveLink to="/T13DControllerConversion">
 		  <HoverGif stillPath="./art/testGifStill.jpg" gifPath="./art/testgif.gif" title="TestTitle4"> </HoverGif>
 		</InteractiveLink>
-		 | 
 		<InteractiveLink to="/T2LevelEditor">
 		  <HoverGif stillPath="./art/testGifStill.jpg" gifPath="./art/testgif.gif" title="TestTitle5"> </HoverGif>
 		</InteractiveLink>
-		 | 
 		<InteractiveLink to="/T3HotsTracker">
 		  <HoverGif stillPath="./art/testGifStill.jpg" gifPath="./art/testgif.gif" title="TestTitle6"> </HoverGif>
 		</InteractiveLink>
--- src/components/HoverGif.tsx
+++ src/components/HoverGif.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import * as CSS from 'csstype';
 //import Interactive from 'react-interactive';
 import { Link } from 'react-router-dom';
-import './style/hovergif.css';
+import './style/styles.css';
 
 let playingGif = false;
 let _stillPath = "";
@@ -15,15 +15,6 @@ type HoverGifProps = {
 	title: string;
 };
 
-const invisibleShieldStyle = {
-	display: 'none',
-}
-
-const shieldStyle = 
-{
-	display: 'flex';
-}
-
 export const HoverGif: React.VFC<HoverGifProps> = (props) => {
   _stillPath = props.stillPath;
   _gifPath = props.gifPath;
@@ -31,8 +22,8 @@ export const HoverGif: React.VFC<HoverGifProps> = (props) => {
   const [isPlaying, setPlaying] = React.useState(false);
 
   return(
-	<div onMouseEnter={() => setPlaying(true)} onMouseLeave={() => setPlaying(false)}>
-		<div className="shield" style={!isPlaying ? shieldStyle : invisibleShieldStyle}>{props.title}</div>
+	<div className="gridItem" onMouseEnter={() => setPlaying(true)} onMouseLeave={() => setPlaying(false)}>
+		<div className="shield" >{props.title}</div>
 		<img id="projectImg" src={isPlaying ? _gifPath : _stillPath} alt="A test gif"></img>
 	</div>
   );
