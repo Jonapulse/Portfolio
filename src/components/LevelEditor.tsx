ff3536fe451886b32e585b6ebc0dc0135325aa8e
Jonah Pulsipher <jonapulse@gmail.com>
2021-07-13 16:31:27 -0600

    Styling applied to articles

--- src/components/LevelEditor.tsx
+++ src/components/LevelEditor.tsx
@@ -9,12 +9,15 @@ const pageLinkContainerStyle: CSS.Properties = {
 };
 
 export const LevelEditor: React.VFC = () => (
-  <div>
-	  <p>This level editor was developed to assist in the creation of a <a href="https://jonapulse.itch.io/pratfall">3D Unity puzzle game</a> which had the unique requirement of reusing level pieces from stage to stage. The final product includedÅc</p>
-	  <ul>
-		<li>Interface code shared among game objects used in the level editor that implemented the functions needed by the editor but allowed for custom tweaking.</li>
-		<li>Functionality to help the designer, such as level-switching that could be set to immediate or to behave visually as it would in-game, player spawning for testing, and a Åesafe saveÅf functionality that walked the designer through levels before confirming to unintentional level overwriting and other mistakes.</li>
-		<li>An XML parser for loading and saving stage data</li>
-	  </ul>
+  <div className="articleContent">
+	<p className="articleTitle">Level Editor</p>
+	<div className="articleIntro">
+		<p>This level editor was developed to assist in the creation of a <a href="https://jonapulse.itch.io/pratfall">3D Unity puzzle game</a> which had the unique requirement of reusing level pieces from stage to stage. The final product includedÅc</p>
+		<ul>
+			<li>Interface code shared among game objects used in the level editor that implemented the functions needed by the editor but allowed for custom tweaking.</li>
+			<li>Functionality to help the designer, such as level-switching that could be set to immediate or to behave visually as it would in-game, player spawning for testing, and a Åesafe saveÅf functionality that walked the designer through levels before confirming to unintentional level overwriting and other mistakes.</li>
+			<li>An XML parser for loading and saving stage data</li>
+		</ul>
+	</div>
   </div>
 );
--- src/components/SquarePanda.tsx
+++ src/components/SquarePanda.tsx
@@ -10,14 +10,17 @@ const pageLinkContainerStyle: CSS.Properties = {
 
 export const SquarePanda: React.VFC = () => (
   <div className="articleContent">
-	<p className="articleTitle">Square Panda is a Sunnyvale-based Educational Games startup for which I developed software in 2017 and 2018. I joined the team a few months before the initial release of 'Squareland', a hub world players could explore and customize that was used to navigate between learning minigames. While there, I...</p>
-	<ul>
-		<li>From day one, built prototypes for minigames and developed them into their final release versions.</li>
-		<li>Worked with a multinational team to integrate art and production code, helping to maintain consistency across the work of multiple departments.</li>
-		<li>Collaborated with the design lead to brainstorm, develop, and pitch new concepts for minigames.</li>
-		<li>Observed live playtests and derived recommendations based on them.</li>
-		<li>Participated in Agile development, evaluating and assigning tasks for sprints, and reporting in daily stand-ups.</li>
-		<li>Prepared, compiled, and deployed new iOS and Android builds for development and release branches.</li>
-	</ul>
+	<p className="articleTitle">Square Panda</p>
+	<div className="articleIntro">
+		<p>Square Panda is a Sunnyvale-based Educational Games startup for which I developed software in 2017 and 2018. I joined the team a few months before the initial release of 'Squareland', a hub world players could explore and customize that was used to navigate between learning minigames. While there, I...</p>
+		<ul>
+			<li>From day one, built prototypes for minigames and developed them into their final release versions.</li>
+			<li>Worked with a multinational team to integrate art and production code, helping to maintain consistency across the work of multiple departments.</li>
+			<li>Collaborated with the design lead to brainstorm, develop, and pitch new concepts for minigames.</li>
+			<li>Observed live playtests and derived recommendations based on them.</li>
+			<li>Participated in Agile development, evaluating and assigning tasks for sprints, and reporting in daily stand-ups.</li>
+			<li>Prepared, compiled, and deployed new iOS and Android builds for development and release branches.</li>
+		</ul>
+	</div>
   </div>
 );
