9fdd7c1ac60e585b1e17c2cd9043c91e1d93b05b
Jonah Pulsipher <jonapulse@gmail.com>
2023-10-17 10:25:22 -0600

    Analytics test push

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -4,6 +4,7 @@ import { InteractiveLink } from './InteractiveLink';
 import { Code, P } from './UI';
 import { HoverGif } from './HoverGif';
 import './style/styles.css';
+import TagManager from 'react-gtm-module';
 
 const pageLinkContainerStyle: CSS.Properties = {
   display: 'block',
@@ -16,35 +17,47 @@ const RepoReadmeLink: React.VFC = () => (
   </InteractiveLink>
 );
 
-export const Home: React.VFC = () => (
-  <div>
-	<div className="aboutMeContainer">
-		<img className="mePicture" src="./art/portrait.jpg"></img>
-		<p className="aboutMe">Howdy. <br></br> I'm a game developer who's spent the last several years traveling the Pacific Northwest, working in industry and releasing my own projects. On weekends I like to spend time gardening and cooking, both mature, adult hobbies that my younger self would see as signs of impending mortality. Every day we take another step closer to the end, and I'd love to spend that time working for you!</p>
-	</div>
-	<div>
-		<table className="gridContainer">
-			<div className="gridLabel"><img className="label" src="./art/gamesLabel.jpg" style={{width: '17px', height: '121px'}}></img></div>
-			<InteractiveLink to="/LightBorrower">
-			  <HoverGif stillPath="./art/windowTransitionStill.jpg" gifPath="./art/windowTransitionGif.gif" title="Light Borrower" subtitle="Game Lead - Unity"> </HoverGif>
-			</InteractiveLink>
-			<InteractiveLink to="/SquarePanda">
-			  <HoverGif stillPath="./art/spStill.jpg" gifPath="./art/spGif.gif" title="Square Panda" subtitle="Developer - Mobile"> </HoverGif>
-			</InteractiveLink>
-			<InteractiveLink to="/HeartburnBoulevard">
-			  <HoverGif stillPath="./art/hbStill.jpg" gifPath="./art/hbGif.gif" title="Heartburn Boulevard" subtitle="Platformer Physics - Web"> </HoverGif>
-			</InteractiveLink>
-			<div className="gridLabel"><img className="label" src="./art/toolsLabel.jpg" style={{width: '17px', height: '123px'}}></img></div>
-			<InteractiveLink to="/ControllerConversion3D">
-			  <HoverGif stillPath="./art/3drayStill.jpg" gifPath="./art/3drayGif.gif" title="3D Raycast Controller" subtitle="Physics - Unity"> </HoverGif>
-			</InteractiveLink>
-			<InteractiveLink to="/LevelEditor">
-			  <HoverGif stillPath="./art/levelEditorStill.jpg" gifPath="./art/levelEditorGif.gif" title="Level Editor" subtitle="Designer Tools - Unity"> </HoverGif>
-			</InteractiveLink>
-			<InteractiveLink to="/HotsCoach">
-			  <HoverGif stillPath="./art/hotsCoachStill.jpg" gifPath="./art/hotsCoachGif.gif" title="HotsCoach" subtitle="Data Processing - Python"> </HoverGif>
-			</InteractiveLink>
-		</table>
-	</div>
-  </div>
-);
+const tagManagerArgs = {
+	dataLayer: {
+		userId: '001',
+		userProject: 'project',
+		page: 'home'
+	},
+	dataLayerName: 'PageDataLayer'
+}
+
+export const Home: React.VFC = () => {
+	TagManager.dataLayer(tagManagerArgs);
+	return (
+		<div>
+			<div className="aboutMeContainer">
+				<img className="mePicture" src="./art/portrait.jpg"></img>
+				<p className="aboutMe">Howdy. <br></br> I'm a game developer who's spent the last several years traveling the Pacific Northwest, working in industry and releasing my own projects. On weekends I like to spend time gardening and cooking, both mature, adult hobbies that my younger self would see as signs of impending mortality. Every day we take another step closer to the end, and I'd love to spend that time working for you!</p>
+			</div>
+			<div>
+				<table className="gridContainer">
+					<div className="gridLabel"><img className="label" src="./art/gamesLabel.jpg" style={{ width: '17px', height: '121px' }}></img></div>
+					<InteractiveLink to="/LightBorrower">
+						<HoverGif stillPath="./art/windowTransitionStill.jpg" gifPath="./art/windowTransitionGif.gif" title="Light Borrower" subtitle="Game Lead - Unity"> </HoverGif>
+					</InteractiveLink>
+					<InteractiveLink to="/SquarePanda">
+						<HoverGif stillPath="./art/spStill.jpg" gifPath="./art/spGif.gif" title="Square Panda" subtitle="Developer - Mobile"> </HoverGif>
+					</InteractiveLink>
+					<InteractiveLink to="/HeartburnBoulevard">
+						<HoverGif stillPath="./art/hbStill.jpg" gifPath="./art/hbGif.gif" title="Heartburn Boulevard" subtitle="Platformer Physics - Web"> </HoverGif>
+					</InteractiveLink>
+					<div className="gridLabel"><img className="label" src="./art/toolsLabel.jpg" style={{ width: '17px', height: '123px' }}></img></div>
+					<InteractiveLink to="/ControllerConversion3D">
+						<HoverGif stillPath="./art/3drayStill.jpg" gifPath="./art/3drayGif.gif" title="3D Raycast Controller" subtitle="Physics - Unity"> </HoverGif>
+					</InteractiveLink>
+					<InteractiveLink to="/LevelEditor">
+						<HoverGif stillPath="./art/levelEditorStill.jpg" gifPath="./art/levelEditorGif.gif" title="Level Editor" subtitle="Designer Tools - Unity"> </HoverGif>
+					</InteractiveLink>
+					<InteractiveLink to="/HotsCoach">
+						<HoverGif stillPath="./art/hotsCoachStill.jpg" gifPath="./art/hotsCoachGif.gif" title="HotsCoach" subtitle="Data Processing - Python"> </HoverGif>
+					</InteractiveLink>
+				</table>
+			</div>
+		</div>
+	);
+}
